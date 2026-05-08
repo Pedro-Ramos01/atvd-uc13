b71e2c879fd161f5bc53b6dcf24cb79e0e9f84db
KAUAN BORGES SANTOS <kauan.bsantos2@senacsp.edu.br>
2026-05-08 20:49:54 -0300

    Atualizando projeto

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,10 +1,28 @@
+import { PRODUCTS } from "@/utils/data/products";
+import { formarCurrency } from "@/utils/functions/format-currency";
 import { useLocalSearchParams } from "expo-router";
-import { View } from "react-native";
+import { Image, Text, View } from "react-native";
 
 export default function Product() {
   const { id } = useLocalSearchParams();
 
-  console.log(id);
+  const product = PRODUCTS.filter((item) => item.id === id)[0];
 
-  return <View className="flex-1"></View>;
+  return (
+    <View className="flex-1">
+      <Image
+        source={product.cover}
+        className="w-full h-52"
+        resizeMode="cover"
+      />
+      <View className="p-5 mt-8 flex-1">
+        <Text className="text-lime-400 text-2x1 font-heading my-2 ">
+            {formarCurrency(product.price)}
+        </Text>
+        <Text className="text-slate-400 font-body text-base leading-6 mb-6">
+            {product.description}
+        </Text>
+      </View>
+    </View>
+  );
 }
---
+++ src/utils/functions/format-currency.tsx
@@ -0,0 +1,6 @@
+export function formarCurrency(value: number) {
+    return value.toLocaleString("pt-BR",{
+        style: "currency",
+        currency: "BRL",
+    });
+}
